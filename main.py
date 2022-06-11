7d4cd6a3e638eb45bbd2cb6d50b2e72f06098b0d
lenaslinkina <90854592+lenaslinkina@users.noreply.github.com>
2022-06-11 18:52:51 +0500

    вторая

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import random
 import string
 import psycopg2
 from config import host, db_name, password, user
-from datetime import datetime, timedelta, date
-from faker import Faker
+from datetime import datetime, timedelta
+from variables import lett, lettEnd, letters, single_random_choice, rand_string
+
 try:
     connection = psycopg2.connect(
         host=host,
@@ -29,16 +30,8 @@ try:
             """
         )
     #     print("[INFO] Table created successfully")
-    fake = Faker()
 
-    print(fake.name())
-    lett = 'ЦУКЕНГШЩЗХФВАПРОЛДЖЭЯЧСМИТБЮ'
-    lettEnd = ['АЯ', 'ОЕ']
-    letters = string.ascii_uppercase
-    arrays = ["ewfer", "sdsdf", "wwdwdwe"]
-    single_random_choice = numpy.random.choice(arrays, size=1)
-    print("один случайный выбор из массива 1-D", single_random_choice, "^^^", ''.join(random.choice(letters) for i in range(1)))
-    rand_string = ''.join(random.choice(letters) for i in range(1))
+
 
 
 #Заполнение дат
@@ -48,7 +41,7 @@ try:
     start = datetime(min_year, 1, 1, 00, 00, 00)
     years = max_year - min_year + 1
     end = start + timedelta(days=365 * years)
-    countDate = random.randint(1, 50)
+
 
 
 #Запрос должен вкелючить все строки
@@ -58,26 +51,27 @@ try:
         lenghtCode = random.randint(5, 20)
         code = "GGA_" + ''.join(random.choice(letters) for i in range(lenghtCode))
         codeArr[i] = "GGA_" + code
-        ranges = random.randint(10, 40)
+        fieldCount = random.randint(10, 40)
         tipdan="none"
-        for i in range(0, ranges):
+        for l in range(0, fieldCount):
             lenghtCode = random.randint(5, 13)
             field = ''.join(random.choice(lett) for i in range(lenghtCode)) + random.choice(lettEnd)
-            countSkv = random.randint(10, 500)
-            for i in range(0, countSkv):
+            countWell = random.randint(10, 500)
+            for j in range(0, countWell):
                 lenghtCode = random.randint(1, 4)
                 well = (str(random.randint(1, 99)) + ''.join(random.choice(letters) for i in range(lenghtCode)))
                 wellCount = random.randint(3, 300)
                 for k in range(0, wellCount):
                     lenghtCode = random.randint(1, 4)
                     path = "C:/Users/lena/Documents/"+str(''.join(random.choice(letters) for i in range(5))) + "\\" +  ''.join(random.choice(letters) for i in range(5))
-                    random_date = start + (end - start) * random.random()
-
-                    with connection.cursor() as cursor:
-                        cursor.execute(
+                    countDate = random.randint(1, 2)
+                    for u in range(0, countDate):
+                        random_date = start + (end - start) * random.random()
+                        with connection.cursor() as cursor:
+                            cursor.execute(
                             f"""Insert into test (Code, Field, TypeData, Well, PathFile, Dates) VALUES
                             ('{code}', '{field}', '{tipdan}', '{well}', '{path}', '{random_date}');"""
-                        )
+                            )
 finally:
     if connection:
         connection.close()
