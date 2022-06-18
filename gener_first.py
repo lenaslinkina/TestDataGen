295e70bd9b18679fa37fc2605bea511d866b16c0
lenaslinkina <90854592+lenaslinkina@users.noreply.github.com>
2022-06-18 11:40:07 +0500

    Добавлен декоратор

--- gener_first.py
+++ gener_first.py
@@ -1,7 +1,10 @@
+
 import random
 import psycopg2
 import functions
 from variables import lett, start, end, codes
+import time_memory
+@time_memory.profile
 def general_first(connection):
     exists = False
     try:
@@ -11,13 +14,13 @@ def general_first(connection):
             code ="GGA_" + ''.join(random.choice(codes) for i in range(1))
            # for l in range(0, len(fields)):
             # пока пусть 100 остается
-            for f in range(0, 100):
-                field = fields[f]
-                countWell = random.randint(10, 200)
+            for f in range(0, 2):
+                field = fields[f].get_text()
+                countWell = random.randint(10, 20)
                 for w in range(0, countWell):
                     well = (str(w)) + ''.join(random.choice(lett) for i in range(1))
                     type = functions.type()
-                    countDate = random.randint(3, 20)
+                    countDate = random.randint(3, 4)
                     for k in range(0, countDate):
                        random_date = start + (end - start) * random.random()
                        path = f"{code}\{field}\{well}\{type}\{random_date}\\" + "filename.ext"
--- main.py
+++ main.py
@@ -1,31 +1,29 @@
+if __name__ == '__main__':
+    import psycopg2
+    import functions
+    from config import host, db_name, password, user
+    from gener_first import general_first
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password= password,
+            database=db_name
+        )
+        connection.autocommit = True
+        exists = functions.table_exists(connection)
 
-import os
-import psycopg2
-import functions
-from config import host, db_name, password, user
-from gener_first import general_first
-try:
-    connection = psycopg2.connect(
-        host=host,
-        user=user,
-        password= password,
-        database=db_name
-    )
+        if (exists):
+            functions.drop_table(connection)
+            functions.create_table(connection)
+        else:
+            functions.create_table(connection)
+            print("[INFO] Table created successfully")
+        general_first(connection)
+        print("End insert")
 
-    connection.autocommit = True
-    exists = functions.table_exists(connection)
-
-    if (exists):
-        functions.drop_table(connection)
-        functions.create_table(connection)
-    else:
-        functions.create_table(connection)
-        print("[INFO] Table created successfully")
-
-    general_first(connection)
-    print("End insert")
-                        # тут бы вывести сообщение о завершении и результаты измерений
-finally:
-    if connection:
-        connection.close()
-        print("[INFO] PostgreSQL connection closed")
+                            # тут бы вывести сообщение о завершении и результаты измерений
+    finally:
+        if connection:
+            connection.close()
+            print("[INFO] PostgreSQL connection closed")
